59b4a4fe1348aad8aa82a4c8c222e547cd61e431
tem66 <66914613+tem66@users.noreply.github.com>
2020-09-24 14:45:38 +0800

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -5,6 +5,41 @@ document.write('<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pl
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 if(dark){document.write('<style>* {box-sizing: border-box}body{color:rgba(255,255,255,.87);background-color:#333232}.mdui-theme-primary-'+main_color+' .mdui-color-theme{background-color:#232427!important}</style>');}
+
+//悬浮广告
+        <script type="text/javascript"> 
+            lastScrollY = 0; 
+            function heartBeat(){ 
+                  var diffY; 
+                if (document.documentElement && document.documentElement.scrollTop) 
+                diffY = document.documentElement.scrollTop; 
+                else if (document.body) 
+                diffY = document.body.scrollTop 
+                else 
+                {/*Netscape stuff*/}
+                //alert(diffY); 
+                percent=.1*(diffY-lastScrollY); 
+                if(percent>0)percent=Math.ceil(percent); 
+                else percent=Math.floor(percent); 
+              document.getElementById("leftDiv").style.top = parseInt(document.getElementById("leftDiv").style.top)+percent+"px"; 
+              document.getElementById("rightDiv").style.top = parseInt(document.getElementById("leftDiv").style.top)+percent+"px"; 
+                lastScrollY=lastScrollY+percent; 
+                //alert(lastScrollY); 
+            } 
+            //下面这段删除后，对联将不跟随屏幕而移动。 
+             window.setInterval("heartBeat()",1); 
+        </script> 
+
+       //左侧广告 
+            <div id="leftDiv" style="bottom:250px; left:20px; position:absolute;">
+              <a href="http://www.kaokew.com/link/adleft" target="_blank"><img src="http://pic.kaokew.com/adleft.png"  style="max-width:180px;width:100%" ></a>
+            </div> 
+          
+       //右侧广告
+            <div id="rightDiv" style="bottom:250px; right:20px; position:absolute;">
+              <a href="http://www.kaokew.com/link/adleft" target="_blank"><img src="http://pic.kaokew.com/adright.png"  style="max-width:180px;width:100%" ></a>
+            
+
 // Initialize the page and load the necessary resources
 function init(){
     document.siteName = $('title').html();
